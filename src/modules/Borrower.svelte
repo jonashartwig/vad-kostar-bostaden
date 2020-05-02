b14639d0e0bd699182a45b70ea650c13d050afcc
jonashartwig <jonas.hartwig@teliacompany.com>
2020-05-02 21:17:33 +0200

    missing files

---
+++ src/modules/Borrower.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let borrower;
+</script>
+
+<div class="row">
+  <div class="col">
+    <div class="form-label-group">
+      <input id="borrower-{borrower.getId()}-name" class="form-control" placeholder="Name" type="text" bind:value={borrower.name} />
+      <label for="borrower-{borrower.getId()}-name">Name</label>
+    </div>
+  </div>
+  <div class="col">
+    <div class="form-label-group">
+      <input id="borrower-{borrower.getId()}-salary" class="form-control" placeholder="Salary" type="number" bind:value={borrower.salary} />
+      <label for="borrower-{borrower.getId()}-salary">Salary</label>
+    </div>
+  </div>
+  <div class="col">
+    <div class="form-label-group">
+      <input id="borrower-{borrower.getId()}-cash" class="form-control" placeholder="Cash" type="number" bind:value={borrower.downPayment} />
+      <label for="borrower-{borrower.getId()}-cash">Cash</label>
+    </div>
+  </div>
+  <div class="col">
+    <button type="button" class="btn btn-default" on:click={ () => dispatch("remove", borrower.getId()) }>
+      <span class="fas fa-minus"></span>
+    </button>
+  </div>
+</div>
---
+++ src/modules/Estate.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { total as requiredDownPayment } from "../mortgage/down_payment";
+
+  export let state;
+</script>
+
+<div class="row">
+  <div class="col">
+    <h2>Estate</h2>
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    This section contains information about the estate. Please enter the price and the existing pantbrev. Each estate has existing pantbrev.You can use both fields to investigate how your expanses might change.
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    <div class="form-label-group">
+      <input id="estate-price" class="form-control" placeholder="Price" type="text" bind:value={state.price} />
+      <label for="estate-price">Price</label>
+    </div>
+  </div>
+  <div class="col">
+    <div class="form-label-group">
+      <input id="estate-pantbrev" class="form-control" placeholder="Pantbrev" type="text" bind:value={state.pantbrev} />
+      <label for="estate-pantbrev">Pantbrev</label>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    The resulting loan will be: { state.getLoan() }
+    {#if state.getCombinedDownPayment() }
+      The loan is based on the combined down payment from the assets of: {state.getCombinedDownPayment()}
+    {:else}
+      The loan is based on the minimum required downPayment of: {requiredDownPayment(state.price)}
+    {/if}
+  </div>
+</div>
